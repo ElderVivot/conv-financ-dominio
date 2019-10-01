8223c6231863bf825ea3a88ff0b8a3520de47501
PA187 - Elder Vivot Dias <PA187@soma.com>
2019-10-01 17:33:23 -0300

    Implementado a ferramenta pra contas a pagar atraves do extrato do bradesco OFX

---
+++ contas_a_pagar_extrato_bradesco_ofx/bin/engine.awk
@@ -0,0 +1,219 @@
+BEGIN { 
+	FS = "";
+	OFS = ";";
+	
+	system("if exist bin\\*.txt del /q bin\\*.txt")
+	system("if exist entrada\\*.ofx dir /b entrada\\*.ofx > bin\\listaofx.txt")
+	system("if exist entrada\\*.ofc dir /b entrada\\*.ofc >> bin\\listaofx.txt")
+	
+	ArquivosCsv = "bin\\listacsv.txt";
+	ArquivosOfx = "bin\\listaofx.txt";
+	
+	_comp_ini = int(Trim(substr(_comp_ini, 4)) "" substr(_comp_ini, 1, 2))
+	_comp_fim = int(Trim(substr(_comp_fim, 4)) "" substr(_comp_fim, 1, 2))
+	
+	# LE O ARQUIVO OFX AFIM DE PODER COMPARAR O QUE FOI PAGO NO CARTAO COM A PLANILHA DE BAIXA DO CLIENTE
+	while ((getline < ArquivosOfx) > 0) {
+		fileofx = "entrada\\" $0
+		
+		# PRIMEIRO WHILE É PRA VER A ESTRUTURA DO OFX, OU SEJA, SE DE FATO É IGUAL A UM XML OU SE NÃO TEM A TAG FINAL DO XML
+		while ((getline < fileofx) > 0) {
+			
+			ofx_1 = ""
+			ofx_1 = $0
+			ofx_1 = tolower( ofx_1 )
+			ofx_1 = Trim(ofx_1)
+			ofx_1 = Trim(ofx_1)
+			
+			# IGUAL AO XML
+			if( index( ofx_1, "</acctid>" ) > 0 ){
+				EstruturaOFX[fileofx] = 1
+				break
+			} 
+			# NÃO TEM A TAG FINAL DO XML
+			else
+				EstruturaOFX[fileofx] = 2
+			
+		}close(fileofx)
+		
+		# LE O XML E PROCESSA SEUS CAMPOS
+		while ((getline < fileofx) > 0) {
+			
+			ofx = ""
+			ofx = $0
+			ofx = tolower( ofx )
+			ofx = Trim(ofx)
+			ofx = Trim(ofx)
+			
+			# ESTRUTURA COM TAG FINAL
+			if( EstruturaOFX[fileofx] == 1 ){
+				if( substr(ofx, 1, 8) == "<bankid>" ){
+					num_banco = upperCase(selecionaTAG( ofx, "<bankid>", "</bankid>" ) )
+					num_banco = int(num_banco)
+				}
+				
+				if( substr(ofx, 1, 8) == "<acctid>" ){
+					conta_corrente = upperCase( selecionaTAG( ofx, "<acctid>", "</acctid>" ) )
+					conta_corrente_int = int( soNumeros(conta_corrente) )
+					if( int(num_banco) == 341 ){
+						conta_corrente = substr(conta_corrente, 5, length(conta_corrente) - 4) "-" substr(conta_corrente, length(conta_corrente), 1)
+					}
+				}
+				
+				if( substr(ofx, 1, 9) == "<trntype>" )
+					tipo_mov = upperCase( selecionaTAG( ofx, "<trntype>", "</trntype>" ) )
+				
+				if( substr(ofx, 1, 10) == "<dtposted>" ){
+					data_mov = selecionaTAG( ofx, "<dtposted>", "</dtposted>" )
+					data_mov = substr(data_mov, 1, 8)
+					data_mov = substr(data_mov, 7, 2) "/" substr(data_mov, 5, 2) "/" substr(data_mov, 1, 4)
+				}
+				
+				if( substr(ofx, 1, 8) == "<trnamt>" ){
+					valor_transacao = selecionaTAG( ofx, "<trnamt>", "</trnamt>" )
+					
+					operacao = ""
+					if(substr(valor_transacao, 1, 1) == "-")
+						operacao = "-"
+					else
+						operacao = "+"
+					
+					gsub("-", "", valor_transacao)
+					gsub("[.]", ",", valor_transacao)
+				}
+				
+				if( substr(ofx, 1, 10) == "<checknum>" ){
+					num_doc = selecionaTAG( ofx, "<checknum>", "</checknum>" )
+					num_doc = int(num_doc)
+				}
+				
+				if( substr(ofx, 1, 6) == "<memo>" )
+					historico = upperCase( selecionaTAG( ofx, "<memo>", "</memo>" ) )
+				
+				if( substr(ofx, 1, 10) == "</stmttrn>" ){
+					print num_banco, conta_corrente, tipo_mov, data_mov, operacao, valor_transacao, num_doc, historico >> "temp\\extrato_cartao.csv"
+				}
+			
+			}
+			
+			# ESTRUTURA SEM TAG FINAL
+			else{
+				if( substr(ofx, 1, 8) == "<bankid>" ){
+					num_banco = upperCase( substr( ofx, 9 , length(ofx) - 8 ) )
+					num_banco = int(num_banco)
+				}
+				
+				if( substr(ofx, 1, 8) == "<acctid>" ){
+					conta_corrente = upperCase( substr( ofx, 9 , length(ofx) - 8 ) )
+					conta_corrente_int = int( soNumeros(conta_corrente) )
+					if( int(num_banco) == 341 ){
+						conta_corrente = substr(conta_corrente, 5, length(conta_corrente) - 4) "-" substr(conta_corrente, length(conta_corrente), 1)
+					}
+				}
+				
+				if( substr(ofx, 1, 9) == "<trntype>" )
+					tipo_mov = upperCase( substr( ofx, 10 , length(ofx) - 9 ) )
+				
+				if( substr(ofx, 1, 10) == "<dtposted>" ){
+					data_mov = substr( ofx, 11 , length(ofx) - 10 )
+					data_mov = substr(data_mov, 1, 8)
+					data_mov = substr(data_mov, 7, 2) "/" substr(data_mov, 5, 2) "/" substr(data_mov, 1, 4)
+				}
+				
+				if( substr(ofx, 1, 8) == "<trnamt>" ){
+					valor_transacao = substr( ofx, 9 , length(ofx) - 8 )
+					
+					operacao = ""
+					if(substr(valor_transacao, 1, 1) == "-")
+						operacao = "-"
+					else
+						operacao = "+"
+					
+					gsub("-", "", valor_transacao)
+					gsub("[.]", ",", valor_transacao)
+				}
+				
+				if( substr(ofx, 1, 10) == "<checknum>" ){
+					num_doc = substr( ofx, 11 , length(ofx) - 10 )
+					num_doc = int(num_doc)
+				}
+				
+				if( substr(ofx, 1, 6) == "<memo>" )
+					historico = upperCase( substr( ofx, 7 , length(ofx) - 6 ) )
+				
+				if( substr(ofx, 1, 10) == "</stmttrn>" ){
+					print num_banco, conta_corrente, tipo_mov, data_mov, operacao, valor_transacao, num_doc, historico >> "temp\\extrato_cartao.csv"
+				}
+			}
+			
+		} close(fileofx)
+	} close(ArquivosOfx)
+	
+	FS = ";"; 
+	OFS = FS;
+
+	print "Documento;Nome Fornecedor;CNPJ Fornecedor;Emissao;Vencimento;Banco Planilha;Banco Oco. Extrato;Data Pagto;Data Oco. Extrato;Valor Pago;Valor Desconto;Valor Juros;Valor Multa;Numero Titulo;Empresa;Codigo Conta Dominio;OBS;Tipo Pagto;Categoria" >> "temp\\pagtos_agrupados.csv"
+	print "Documento;Nome Cliente;CNPJ Cliente;Emissao;Vencimento;Banco Planilha;Banco Oco. Extrato;Data Pagto;Data Oco. Extrato;Valor Pago;Valor Desconto;Valor Juros;Valor Multa;Numero Titulo;Empresa;Codigo Conta Dominio;OBS;Tipo Pagto;Categoria" >> "temp\\recebtos_agrupados.csv"
+	
+	while ( (getline < "temp\\extrato_cartao.csv") > 0 ) {
+		num_banco_extrato = $1
+		conta_corrente_extrato = $2
+		tipo_mov_extrato = $3
+
+		data_mov_extrato = $4
+		data_mov_int = int(substr(data_mov_extrato, 7) "" substr(data_mov_extrato, 4, 2))
+
+		operacao_extrato = $5
+		valor_transacao_extrato = $6
+		num_doc_extrato = $7
+
+		historico_extrato = $8
+		if( index(historico_extrato, "COBRANCA") > 0 && index(historico_extrato, "ELETRON") > 0 ){
+			historico_extrato = substr(historico_extrato, index(historico_extrato, "COBRANCA") + 8 )
+			historico_extrato = Trim(historico_extrato)
+		}
+
+		if( index(historico_extrato, "FORNECEDOR") > 0 && index(historico_extrato, "RECEBIMENTO") > 0 ){
+			historico_extrato = substr(historico_extrato, index(historico_extrato, "FORNECEDOR") + 10 )
+			historico_extrato = Trim(historico_extrato)
+		}
+
+		historico_complementar_extrato = $9
+
+		ExisteMov[operacao_extrato, data_mov_extrato, valor_transacao_extrato] = 1
+		BancoPago[operacao_extrato, data_mov_extrato, valor_transacao_extrato] = num_banco_extrato "-" conta_corrente_extrato
+		DataPagto[operacao_extrato, data_mov_extrato, valor_transacao_extrato] = data_mov_extrato
+		
+		ExisteMovBanco[num_banco_extrato, operacao_extrato, data_mov_extrato, valor_transacao_extrato] = 1
+		BancoPagoBanco[num_banco_extrato, operacao_extrato, data_mov_extrato, valor_transacao_extrato] = num_banco_extrato "-" conta_corrente_extrato
+		DataPagtoBanco[num_banco_extrato, operacao_extrato, data_mov_extrato, valor_transacao_extrato] = data_mov_extrato
+		
+		# QUANDO É CHEQUE GUARDA A DATA QUE O CHEQUE COMPENSOU, É ELA QUE TEM QUE SER UTILIZADA COMO DATA DA BAIXA
+		if( index(historico_extrato, "CHEQ COMP") > 0 || index(historico_extrato, "CHEQUE SAC") > 0 ){
+			DataCompensacaoCheque[num_doc_extrato] = data_mov_extrato
+			BancoPagoCheque[num_doc_extrato] = num_banco_extrato "-" conta_corrente_extrato
+		}
+
+		forn_cli = historico_extrato
+		banco_arquivo = "BRADESCO-" conta_corrente_extrato
+		baixa = data_mov_extrato
+		valor_pago = valor_transacao_extrato
+		obs = historico_extrato
+
+		# PAGOS
+		if( operacao_extrato == "-" && _comp_ini <= data_mov_int && data_mov_int <= _comp_fim ){
+			print nota, forn_cli, "'" cnpj_forn_cli, emissao, vencimento, banco_arquivo, banco, baixa, baixa_extrato, valor_pago, 
+					valor_desconto, valor_juros, valor_multa, nota_completo_orig, codi_emp, "", obs, tipo_pagto, categoria >> "temp\\pagtos_agrupados.csv"
+		}
+
+		# RECEBIMENTOS
+		if( operacao_extrato == "+" && _comp_ini <= data_mov_int && data_mov_int <= _comp_fim ){
+			print nota, forn_cli, "'" cnpj_forn_cli, emissao, vencimento, banco_arquivo, banco, baixa, baixa_extrato, valor_pago, 
+					valor_desconto, valor_juros, valor_multa, nota_completo_orig, codi_emp, "", obs, tipo_pagto, categoria >> "temp\\recebtos_agrupados.csv"
+		}
+
+	} close("temp\\extrato_cartao.csv")
+	
+	
+	system("if exist bin\\*.txt del /q bin\\*.txt")
+}
---
+++ contas_a_pagar_extrato_bradesco_ofx/bin/leArquivos.py
@@ -0,0 +1,261 @@
+import xlrd
+import os
+import unicodedata
+import re
+import csv
+import time
+import sys
+#import openpyxl
+#import pandas as pd
+import datetime
+import funcoesUteis
+import platform
+
+def buscaArquivosEmPasta(caminho="", extensao=(".XLS", "XLSX")):
+    arquivos = os.listdir(caminho)
+    lista_arquivos = []
+
+    for arquivo in arquivos:
+        arquivo = str(arquivo).upper()
+        if arquivo.endswith(extensao):
+            lista_arquivos.append(caminho+"\\"+arquivo)
+
+    return lista_arquivos
+
+def removerAcentosECaracteresEspeciais(palavra):
+    # Unicode normalize transforma um caracter em seu equivalente em latin.
+    nfkd = unicodedata.normalize('NFKD', palavra).encode('ASCII', 'ignore').decode('ASCII')
+    palavraTratada = u"".join([c for c in nfkd if not unicodedata.combining(c)])
+
+    # Usa expressão regular para retornar a palavra apenas com valores corretos
+    return re.sub('[^a-zA-Z0-9.!+:=)$(/*,\-_ \\\]', '', palavraTratada)
+
+def PDFToText(arquivos=buscaArquivosEmPasta(caminho="entrada",extensao=(".PDF")), mode = "simple"):
+    for arquivo in arquivos:
+        nome_arquivo = os.path.basename(arquivo)
+        saida = "temp\\" + str(nome_arquivo[0:len(nome_arquivo)-4]) + ".txt"
+        try:
+
+            # verifica se o Windows é 32 ou 64 bits
+            architecture = platform.architecture()
+            if architecture[0].count('32') > 0:
+                pdftotext = "pdftotext32.exe"
+            else:
+                pdftotext = "pdftotext64.exe"
+            
+            # chama o comando pra transformação do PDF
+            comando = f"bin\\{pdftotext} -{mode} \"{arquivo}\" \"{saida}\""
+            os.system(comando)
+        except Exception as ex:
+            print(f"Nao foi possivel transformar o arquivo \"{saida}\". O erro é: {str(ex)}")
+
+# chama a geração da transformação pra PDF
+PDFToText()
+
+def leLinhasExtrato(arquivos=buscaArquivosEmPasta(caminho="temp", extensao=(".TXT"))):
+    lista_arquivos = {}
+    lista_linha = []
+    
+    for arquivo in arquivos:
+        # pra cada arquivo criar uma posição no dicionário
+        lista_arquivos[arquivo] = lista_linha[:]
+        
+        # le o arquivo e grava num vetor
+        with open(arquivo, 'rt') as txtfile:
+            for linha in txtfile:
+                linha = str(linha).replace("\n", "")
+                # ignora linhas totalmente em branco
+                if(funcoesUteis.trataCampoTexto(linha) == ""):
+                    continue
+                lista_linha.append(linha)
+            lista_arquivos[arquivo] = lista_linha[:]
+            lista_linha.clear()
+        txtfile.close()
+
+    return lista_arquivos
+
+def organizaExtrato(saida="temp\\baixas.csv"):
+    saida = open(saida, "w", encoding='utf-8')
+    saida.write("Data;Documento;Historico;Historico Complementar;Valor;Operacao\n")
+
+    lista_arquivos = leLinhasExtrato()
+    
+    for linhas in lista_arquivos.values():
+
+        posicao_historico = 0
+        posicao_data = 0
+
+        data = datetime.datetime.strptime("01/01/1900", "%d/%m/%Y").date()
+        data = data.strftime("%d/%m/%Y")
+        documento = ""
+        valor = 0
+        operador = ""
+        fornecedor_cliente = ""
+        historico = ""
+
+        for num_row, row in enumerate(linhas):
+
+            posicao_data_temp = str(row).upper().find("DT.")
+            if posicao_data_temp > 0:
+                if posicao_data_temp > 5:
+                    posicao_data = posicao_data_temp-5
+                else:
+                    posicao_data = 0
+
+            # lê a próxima linha pra saber se é uma linha com complementação dos dados da atual ou não
+            try:
+                proxima_linha = linhas[num_row+1]
+            except Exception:
+                proxima_linha = ""
+
+            data_temp_proxima_linha = proxima_linha[posicao_data:posicao_data+17]
+            data_temp_proxima_linha = funcoesUteis.retornaCampoComoData(data_temp_proxima_linha)
+
+            # ---- começa o tratamento de cada campo ----
+            data_temp = row[posicao_data:posicao_data+17]
+            data_temp = funcoesUteis.retornaCampoComoData(data_temp)
+            # verifica se é uma data válida pra começar os tratamentos de cada campo
+            if data_temp is not None:
+                data = funcoesUteis.transformaCampoDataParaFormatoBrasileiro(data_temp)
+
+                # ignora linhas que não são de pagamentos
+                if data == "01/01/1900":
+                    continue
+
+                # retira os espaços excessivos
+                row = funcoesUteis.trataCampoTexto(row)
+
+                # divide as linhas em espaço pois as posições do BB variam muito, então vamos pegar os dados de acordo a posição base deles (agencia e tipo_operacao(D,C) )
+                row_dividida = row.split()
+                #print(row_dividida)
+
+                # vai servir apenas pra identificar em qual posição está a agência
+                data_movimento = row_dividida[1]
+                data_movimento = funcoesUteis.retornaCampoComoData(data_movimento)
+
+                # serve pra sabermos onde começar o processamento do histórico
+                posicao_agencia = 0
+                if data_movimento is None:
+                    posicao_agencia = 1
+                else:
+                    posicao_agencia = 2
+
+                # ------ INICIO POSICAO_OPERACAO - serve pra sabermos até onde o histórico vai
+                try:
+                    posicao_operacao_debito = row_dividida.index('D')
+                except Exception:
+                    posicao_operacao_debito = 0
+                try:
+                    posicao_operacao_credito = row_dividida.index('C')
+                except Exception:
+                    posicao_operacao_credito = 0
+                
+                posicao_operacao = 0
+                
+                if posicao_operacao_debito > 0:
+                    if posicao_operacao_credito == 0:
+                        posicao_operacao = posicao_operacao_debito
+                    else:
+                        if posicao_operacao_debito <= posicao_operacao_credito:
+                            posicao_operacao = posicao_operacao_debito
+                        else:
+                            posicao_operacao = posicao_operacao_credito
+                if posicao_operacao_credito > 0:
+                    if posicao_operacao_debito == 0:
+                        posicao_operacao = posicao_operacao_credito
+                    else:
+                        if posicao_operacao_credito <= posicao_operacao_debito:
+                            posicao_operacao = posicao_operacao_credito
+                        else:
+                            posicao_operacao = posicao_operacao_debito
+                # ------------- FIM POSICAO_OPERACAO
+
+                primeiro_campo_historico = funcoesUteis.trocaCaracteresTextoPraLetraX(row_dividida[posicao_agencia+2])
+                if primeiro_campo_historico.count('X') > 0:
+                    posicao_inicio = posicao_agencia+2
+                else:
+                    posicao_inicio = posicao_agencia+3
+                
+                historico = ""
+                for i in range(posicao_inicio,posicao_operacao-2):
+                    historico = historico + " " + funcoesUteis.trataCampoTexto(row_dividida[i])
+                historico = historico.strip()
+
+                # ignora as linhas que são referente à saldos
+                if historico.count("SALDO") > 0:
+                    continue
+
+                documento = funcoesUteis.trataCampoTexto(row_dividida[posicao_operacao-2])
+
+                valor = funcoesUteis.trataCampoDecimal(row_dividida[posicao_operacao-1])
+                
+                operador = row_dividida[posicao_operacao]
+                if operador == "D":
+                   operador = "-"
+                else:
+                   operador = "+"
+                
+                fornecedor_cliente = ""
+
+                # primeira geração dos dados quando todas as informações estão em uma linha apenas, ou seja, a próxima linha também já outro campo com data
+                if data_temp_proxima_linha is not None and valor > 0:
+                    saida.write(f"{data};{documento};{historico};{fornecedor_cliente};{valor:.2f};{operador}\n")
+            
+            # DAQUI PRA BAIXO analisando a complementação do depósito
+            posicao_historico_temp = row.upper().find("HISTÓRICO")
+            if posicao_historico_temp > 0:
+                posicao_historico = posicao_historico_temp - 10 # pega 10 posições a menos pra questão de 'segurança'
+
+            historico_temp = funcoesUteis.trataCampoTexto(row[posicao_historico:posicao_historico+65])
+
+            fornecedor_cliente_temp = ""
+            # segunda geração dos dados quando as informações complementares está em APENAS uma LINHA ABAIXO
+            if data_temp is None and historico_temp != "" and data_temp_proxima_linha is None and valor > 0:
+                fornecedor_cliente_temp = fornecedor_cliente_temp + " " + historico_temp
+                fornecedor_cliente_temp = fornecedor_cliente_temp.strip()
+
+                if historico.count('TED') > 0 or historico.count('TRANSF') > 0 or historico.count('DOC CR') > 0:
+                    fornecedor_cliente_dividido = fornecedor_cliente_temp.split()
+
+                    for campo in fornecedor_cliente_dividido:
+                        if funcoesUteis.trocaCaracteresTextoPraLetraX(campo).count('X') > 0:
+                            fornecedor_cliente = fornecedor_cliente + " " + campo
+
+                    fornecedor_cliente = fornecedor_cliente.strip()
+                else:
+                    fornecedor_cliente = fornecedor_cliente_temp
+
+                # analisa se na verdade é um histórico válido, pois pode ser uma linha que contenha um tanto de carecter que não serve pra nada. Então considera como
+                # histórico somente se conter o historico_temp válido
+                historico_temp_inicio = funcoesUteis.trataCampoTexto(row[0:posicao_historico])
+                if len(historico_temp_inicio) > 0:
+                    fornecedor_cliente = ""
+
+            # terceira geração dos dados quando as informações complementares está em MAIS DE UMA LINHA ABAIXO
+            if data_temp is None and historico_temp != "" and data_temp_proxima_linha is not None and valor > 0:
+                fornecedor_cliente_temp = fornecedor_cliente_temp + " " + historico_temp
+                fornecedor_cliente_temp = fornecedor_cliente_temp.strip()
+
+                if historico.count('TED') > 0 or historico.count('TRANSF') > 0 or historico.count('DOC CR') > 0:
+                    fornecedor_cliente_dividido = fornecedor_cliente_temp.split()
+
+                    for campo in fornecedor_cliente_dividido:
+                        if funcoesUteis.trocaCaracteresTextoPraLetraX(campo).count('X') > 0:
+                            fornecedor_cliente = fornecedor_cliente + " " + campo
+
+                    fornecedor_cliente = fornecedor_cliente.strip()
+                else:
+                    fornecedor_cliente = fornecedor_cliente_temp
+                
+                # analisa se na verdade é um histórico válido, pois pode ser uma linha que contenha um tanto de carecter que não serve pra nada. Então considera como
+                # histórico somente se conter o historico_temp válido
+                historico_temp_inicio = funcoesUteis.trataCampoTexto(row[0:posicao_historico])
+                if len(historico_temp_inicio) > 0:
+                    fornecedor_cliente = ""
+
+                saida.write(f"{data};{documento};{historico};{fornecedor_cliente};{valor:.2f};{operador}\n")
+                
+    saida.close()
+
+# chama a geração pra organizar o extrato
+organizaExtrato()
